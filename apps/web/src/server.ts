3296b4894c23c59b096f5201f98c41cdec007793
Vladimír Čajka <cajka.vlada@gmail.com>
2025-08-17 13:34:52 +0200

    #65/overwritinggetUrlOrigin in server

--- apps/web/src/server.ts
+++ apps/web/src/server.ts
@@ -6,14 +6,18 @@ import {
 
 import { createClerkHandler } from "@gc/auth";
 
-import { overwriteGetLocale } from "./paraglide/runtime.js";
+import {
+  overwriteGetLocale,
+  overwriteGetUrlOrigin,
+} from "./paraglide/runtime.js";
 import { paraglideMiddleware } from "./paraglide/server.js";
 import { createRouter } from "./router";
 
 export default createClerkHandler(createStartHandler({ createRouter }))(
   (event) =>
-    paraglideMiddleware(getWebRequest(), ({ locale }) => {
+    paraglideMiddleware(getWebRequest(), ({ locale, request }) => {
       overwriteGetLocale(() => locale);
+      overwriteGetUrlOrigin(() => new URL(request.url).origin);
       return defaultStreamHandler(event);
     })
 );
