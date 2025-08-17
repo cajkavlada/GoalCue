5516405b9446dd0a8f229e700f2af7581bf430f5
Vladimír Čajka <cajka.vlada@gmail.com>
2025-08-17 12:07:02 +0200

    #65/readding paraglide middleware to server.ts

--- apps/web/src/server.ts
+++ apps/web/src/server.ts
@@ -1,21 +1,19 @@
 import {
   createStartHandler,
   defaultStreamHandler,
-  // getWebRequest,
+  getWebRequest,
 } from "@tanstack/react-start/server";
 
 import { createClerkHandler } from "@gc/auth";
 
-// import { overwriteGetLocale } from "./paraglide/runtime.js";
-// import { paraglideMiddleware } from "./paraglide/server.js";
+import { overwriteGetLocale } from "./paraglide/runtime.js";
+import { paraglideMiddleware } from "./paraglide/server.js";
 import { createRouter } from "./router";
 
 export default createClerkHandler(createStartHandler({ createRouter }))(
-  defaultStreamHandler
+  (event) =>
+    paraglideMiddleware(getWebRequest(), ({ locale }) => {
+      overwriteGetLocale(() => locale);
+      return defaultStreamHandler(event);
+    })
 );
-//   (event) =>
-//     paraglideMiddleware(getWebRequest(), ({ locale }) => {
-//       overwriteGetLocale(() => locale);
-//       return defaultStreamHandler(event);
-//     })
-// );
