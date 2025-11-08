40ab69098ada7093c50f8219af15fb0da76bd920
Vladimír Čajka <cajka.vlada@gmail.com>
2025-11-08 01:30:23 +0100

    Priority classes editing Fixes #174

---
+++ apps/web/src/features/priority-classes/priority-class-edit-drawer.tsx
@@ -0,0 +1,72 @@
+import { useAppForm } from "@gc/form";
+import { m } from "@gc/i18n/messages";
+import { ErrorSuspense } from "@gc/react-kit";
+import { Drawer } from "@gc/ui";
+import {
+  PriorityClass,
+  UpdatePriorityClassArgs,
+  updatePriorityClassZodSchema,
+} from "@gc/validators";
+
+import { useUpdatePriorityClass } from "./use-priority-classes";
+
+export function PriorityClassEditDrawer({
+  editedPriorityClass,
+}: {
+  editedPriorityClass: PriorityClass;
+}) {
+  return (
+    <Drawer
+      title={m.priorityClasses_edit_drawer_title()}
+      description={m.priorityClasses_edit_drawer_description()}
+      customFooter
+    >
+      <ErrorSuspense>
+        <PriorityClassEditForm editedPriorityClass={editedPriorityClass} />
+      </ErrorSuspense>
+    </Drawer>
+  );
+}
+
+export function PriorityClassEditForm({
+  editedPriorityClass,
+}: {
+  editedPriorityClass: PriorityClass;
+}) {
+  const updateMutation = useUpdatePriorityClass();
+
+  const defaultValues: UpdatePriorityClassArgs = {
+    name: editedPriorityClass.name,
+  };
+
+  const form = useAppForm({
+    defaultValues,
+    validators: {
+      onBlur: updatePriorityClassZodSchema,
+    },
+    onSubmit: async ({ value }) => {
+      await updateMutation.mutateAsync({
+        priorityClassId: editedPriorityClass._id,
+        ...value,
+      });
+    },
+  });
+
+  return (
+    <form.AppForm>
+      <form.FormRoot className="flex flex-col gap-4">
+        <form.AppField name="name">
+          {(field) => (
+            <field.Input
+              label={m.priorityClasses_form_field_name_label()}
+              autoFocus
+            />
+          )}
+        </form.AppField>
+        <Drawer.Footer>
+          <form.SubmitButton />
+        </Drawer.Footer>
+      </form.FormRoot>
+    </form.AppForm>
+  );
+}
--- apps/web/src/features/task-types/task-type-list.tsx
+++ apps/web/src/features/task-types/task-type-list.tsx
@@ -26,7 +26,7 @@ export function TaskTypeList({
       )}
       {taskTypes.length > 0 && (
         <>
-          <div className="flex h-[36px] items-center gap-2 pr-2">
+          <div className="flex h-[36px] items-center gap-2 pl-1 pr-2">
             <Checkbox
               checked={isAllSelected}
               onCheckedChange={toggleSelectAll}
